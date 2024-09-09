7fe326a71fb5a9d1acf3f69359e84da52090925a
sreeja842 <anandha.krishnan@urolime.com>
2024-09-09 23:25:47 +0530

    Change allowed host

--- multi_company_management/settings.py
+++ multi_company_management/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2lr(c=8p#67nix*)=jt6kxl_x1ftjbb%7z!ud%rek#vw!-1qy4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [] 
 
 
 # Application definition
